03d41045f1a2d06483b69dac84843358bed93c87
Stephen Garabedian <spg1502@rit.edu>
2017-09-05 19:28:05 -0400

    Initial commit

---
+++ OverwatchTeamCompGrader/Main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello and welcome to the Overwatch Team Comp Grader" << endl;
+	cout << "To use this program, input the names of each of the 6 members of your Overwatch team, and this program will tell you how fit your team is for victory!" << endl;
+	cout << "Please enter hero names with their correct spelling, capitalization, and punctuaion (as in D.Va)" << endl;
+}
